9ca1e4110db51c4865fd8588d8a0df909d905544
IcedCrescent <daoquangtrung1998@outlook.com.vn>
2026-04-12 16:24:13 +0700

    Setup for GH Page deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/CBAP_Mock_50Q_55m/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
